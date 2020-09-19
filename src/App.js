0e093f0e04080f4e194f93983413c7d7fdbedbfc
Noshad <noshad01@gmail.com>
2020-09-19 16:54:46 +0530

    monster master

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {CardList} from "./components/card-list/card-list.component";
+//import logo from './logo.svg';
+import './App.css';
+import {SearchBox} from "./components/search-box/search-box.component";
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      monsters:[],
+      searchField :''
+
+    }
+      this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {this.setState({monsters: users})})
+  }
+
+  handleChange(e){
+      this.setState({searchField: e.target.value});
+  }
+  render(){
+
+    const {monsters, searchField} = this.state;
+    const filterMonsters = monsters.filter(monsters =>
+      monsters.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    return(
+      <div className='App'>
+          <h1 className='logo-h1'>Monsters Rolodex</h1>
+        <SearchBox
+            placeholder='search monsters'
+            handleChange={this.handleChange}
+        />
+        <CardList monsters={filterMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
